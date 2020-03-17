7368ae6b170b0197a251a6ec9dd5c9448c4b35a5
ทวีทอง วิวัฒนขจรสุข <thawwiwa@dynastyceramic.com>
2020-03-17 17:43:12 +0700

    update 2020-03-17

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,23 @@
 import { NgModule } from '@angular/core';
+import { ModuleWithProviders }  from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UserComponent } from './user/user.component';
+import { PermissionComponent } from './permission/permission.component';
+import { CustomerComponent } from './customer/customer.component';
+import { CustomerAddComponent } from './customer-add/customer-add.component';
 
+const routes: Routes = [
+  {path: 'customer', component: CustomerComponent},
+  {path: 'customeradd', component: CustomerAddComponent},
+  {path: 'user', component: UserComponent},
+  {path: 'permission', component: PermissionComponent}
+];
 
-const routes: Routes = [];
+export const routing: ModuleWithProviders = RouterModule.forRoot(routes);
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule { 
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { UserComponent } from './user/user.component';
+import { PermissionComponent } from './permission/permission.component';
+import { TabViewModule } from 'primeng/tabview';
+import { routing } from './app-routing.module';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { CustomerComponent } from './customer/customer.component';
+import { CustomerAddComponent } from './customer-add/customer-add.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    PermissionComponent,
+    CustomerComponent,
+    CustomerAddComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    routing,
+    NgbModule,
+    TabViewModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
